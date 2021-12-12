b61ee8b1216728202ba0f033dd7c22fbda56181f
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-12-12 17:05:15 +0700

    api signup

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,15 +1,9 @@
 const Player = require('../player/model');
 const config = require('../../config/index');
-const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 const path = require('path');
 
-cloudinary.config({
-   cloud_name: 'wildanakhmad3584',
-   api_key: '481815712393864',
-   api_secret: 'efhLh69mDdk0vacfQHdj3II6qUM',
-   secure: true,
-})
+
 module.exports = {
     signup: async(req, res, next) => {
         try {
@@ -25,12 +19,6 @@ module.exports = {
                 const dst = fs.createWriteStream(target_path);
 
                 src.pipe(dst);
-
-                cloudinary.uploader.upload(target_path, 
-                    function (error, result) {
-                        console.log(error, result)
-                    }
-                );
                 src.on('end', async () => {
                     try {
                         let player = new Player({...payload, avatar: filename});
